28936bafd215878d7e14219988cb40fd836091aa
pranuthipachigalla <pranuthi@techscapeai.in>
2025-12-17 23:57:20 +0530

    Add StepThree and AccSecure pages and components

---
+++ src/pages/AccSecurePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AccSecure from '../components/AccSecure'
+
+export default function AccSecurePage() {
+    return (
+      <>
+       <AccSecure />
+      </>
+    )
+}
